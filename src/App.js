8909ac1be74af080eb127dc17fa48de17f32c42d
angus <angushtlam@gmail.com>
2018-12-06 18:55:56 -0500

    Restructured Estimator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import Menu from "./components/Menu";
-import Estimator from "./organisms/Estimator";
+import CategorySelection from "./organisms/CategorySelection";
+import LocationSelection from "./organisms/LocationSelection";
+
+const Header = styled.h3`
+  color: #1c1f34;
+  display: inline-block;
+  font-size: 56px;
+  margin-bottom: 68px;
+  white-space: nowrap;
+`;
+
+const Content = styled.div`
+  text-align: center;
+`;
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      category: "Henlo",
+      step: "Category Selection"
+    };
+  }
+
   render() {
+    const { category, step } = this.state;
+
     return (
       <div>
         <Menu />
-        <Estimator />
+        <Content>
+          <Header>Get an estimate</Header>
+          {step === "Category Selection" && (
+            <CategorySelection
+              onSelection={category => () => {
+                this.setState({
+                  category,
+                  step: "Location Selection"
+                });
+              }}
+            />
+          )}
+        </Content>
       </div>
     );
   }
---
+++ src/organisms/CategorySelection.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  GroupedSelection,
+  GroupedSelectionRow,
+  GroupedSelectionItem
+} from "../components/GroupedSelection";
+import CraigslistPickupSvg from "../assets/craigslist_pickup.svg";
+import DonationPickupSvg from "../assets/donation_pickup.svg";
+import JunkRemovalSvg from "../assets/junk_removal.svg";
+import OtherSvg from "../assets/other.svg";
+import SmallMoveSvg from "../assets/small_move.svg";
+import StorageMoveSvg from "../assets/storage_move.svg";
+import StoreDeliverySvg from "../assets/store_delivery.svg";
+
+const StyledGroupedSelection = styled(GroupedSelection)`
+  margin: 0 auto;
+`;
+
+export default ({ onSelection }) => (
+  <StyledGroupedSelection>
+    <GroupedSelectionRow>
+      <GroupedSelectionItem
+        icon={StoreDeliverySvg}
+        label="Store Delivery"
+        onClick={onSelection`Store Delivery`}
+      />
+      <GroupedSelectionItem
+        icon={SmallMoveSvg}
+        label="Small Move"
+        onClick={onSelection`Small Move`}
+      />
+      <GroupedSelectionItem
+        icon={StorageMoveSvg}
+        label="Storage Move"
+        onClick={onSelection`Storage Move`}
+      />
+    </GroupedSelectionRow>
+    <GroupedSelectionRow>
+      <GroupedSelectionItem
+        icon={JunkRemovalSvg}
+        label="Junk Removal"
+        onClick={onSelection`Junk Removal`}
+      />
+      <GroupedSelectionItem
+        icon={CraigslistPickupSvg}
+        label="Craigslist Pickup"
+        onClick={onSelection`Craigslist Pickup`}
+      />
+      <GroupedSelectionItem
+        icon={DonationPickupSvg}
+        label="Donation Pickup"
+        onClick={onSelection`Donation Pickup`}
+      />
+    </GroupedSelectionRow>
+    <GroupedSelectionRow height="120px">
+      <GroupedSelectionItem
+        icon={OtherSvg}
+        label="Other"
+        onClick={onSelection`Other`}
+      />
+    </GroupedSelectionRow>
+  </StyledGroupedSelection>
+);
