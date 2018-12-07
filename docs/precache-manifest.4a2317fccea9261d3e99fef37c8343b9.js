ec5c3d852b9512b3e64c1b57df1010e5755f8e9a
angus <angushtlam@gmail.com>
2018-12-07 10:09:10 -0500

    Host project on GitHub Pages via docs folder

---
+++ docs/precache-manifest.4a2317fccea9261d3e99fef37c8343b9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4a7ac28972581575ae9414a41bde6e8",
+    "url": "/static/media/storage_move.d4a7ac28.svg"
+  },
+  {
+    "revision": "a2cc7dc79630f7daa360",
+    "url": "/static/js/main.a2cc7dc7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b20677bcb4ef77c4598215d8a5e8b299",
+    "url": "/static/media/logo.b20677bc.svg"
+  },
+  {
+    "revision": "269ad6678f0a7170fad92a3031668a11",
+    "url": "/static/media/craigslist_pickup.269ad667.svg"
+  },
+  {
+    "revision": "2b4d487a6e01089d2adb7563bc6e8ffe",
+    "url": "/static/media/donation_pickup.2b4d487a.svg"
+  },
+  {
+    "revision": "c1d45024986d5f8aeced0ae422508e21",
+    "url": "/static/media/junk_removal.c1d45024.svg"
+  },
+  {
+    "revision": "0c6e30fd5fd04894fdce9cbc297819bc",
+    "url": "/static/media/other.0c6e30fd.svg"
+  },
+  {
+    "revision": "7dd66e287035625dfcb7e5e11dc24054",
+    "url": "/static/media/small_move.7dd66e28.svg"
+  },
+  {
+    "revision": "00e9ce6a6bbb1b865060d3736a237066",
+    "url": "/static/media/van.00e9ce6a.svg"
+  },
+  {
+    "revision": "31b106e10585f1119c429d8c9da1ce2c",
+    "url": "/static/media/store_delivery.31b106e1.svg"
+  },
+  {
+    "revision": "aed59585a56b0868b588c28a56e941be",
+    "url": "/static/media/down_arrow.aed59585.svg"
+  },
+  {
+    "revision": "80b6c57a1e2ee6fe093704388f142a74",
+    "url": "/static/media/up_arrow.80b6c57a.svg"
+  },
+  {
+    "revision": "b2af850cd070bc90e47d3dc488b9e44d",
+    "url": "/static/media/all_our_luggers.b2af850c.svg"
+  },
+  {
+    "revision": "e8247ef302bc6ec63b4f729ed3d039cc",
+    "url": "/static/media/one_lugger.e8247ef3.svg"
+  },
+  {
+    "revision": "9e318d5b55db47a7576f152870b7c4f9",
+    "url": "/static/media/two_luggers.9e318d5b.svg"
+  },
+  {
+    "revision": "49cebe89f4e5a4cc5ff19d957aa2e0ad",
+    "url": "/static/media/pickup.49cebe89.svg"
+  },
+  {
+    "revision": "7c7f6c2f80df812ca6ac",
+    "url": "/static/js/1.7c7f6c2f.chunk.js"
+  },
+  {
+    "revision": "884ea276f74174f2edb4ba447521332a",
+    "url": "/index.html"
+  }
+];
